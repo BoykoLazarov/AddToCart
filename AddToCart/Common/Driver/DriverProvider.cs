883435a103bf0ab0670b990dd401b247d396e1bf
Boyko Lazarov <boiko.lazarov@gmail.com>
2024-02-11 20:23:20 +0200

    extracted browser creation into separate factory class

--- AddToCart/Common/Driver/DriverProvider.cs
+++ AddToCart/Common/Driver/DriverProvider.cs
@@ -1,19 +1,9 @@
-﻿using AddToCart.Common.Utility;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Edge;
-using OpenQA.Selenium.Firefox;
+﻿using OpenQA.Selenium;
 
 namespace AddToCart.Common.Driver
 {
     public class DriverProvider
     {
-        private const string CHROME = "chrome";
-        private const string EDGE = "edge";
-        private const string FIREFOX = "firefox";
-        private const string SCREEN_RESOLUTION = "--window-size=1920,1080";
-        private const string INCOGNITO = "--incognito";
-
         private static readonly ThreadLocal<DriverProvider> DriverProviderInstance = new ThreadLocal<DriverProvider>(() => new DriverProvider(), trackAllValues: true);
         private IWebDriver _driver;
 
@@ -21,52 +11,7 @@ namespace AddToCart.Common.Driver
 
         public static DriverProvider Instance => DriverProviderInstance.Value;
 
-        private static IWebDriver InitializeWebDriver()
-        {
-            string browserName = WebSettingsProvider.GetSettings().BrowserToUse.ToLower();
-            string browser = string.IsNullOrEmpty(browserName) ? CHROME : browserName;
-
-            switch (browser)
-            {
-                case CHROME:
-                    return GetChromeDriverWithOptions();
-
-                case EDGE:
-                    return GetEdgeDriverWithOptions();
-
-                case FIREFOX:
-                    return GetFirefoxDriverWithOptions();
-
-                default:
-                    Console.WriteLine($"Browser '{browser}' is not supported.");
-                    Console.WriteLine($"Starting default '{CHROME}' web driver.");
-                    return GetChromeDriverWithOptions();
-            }
-        }
-
-        private static ChromeDriver GetChromeDriverWithOptions()
-        {
-            ChromeOptions chromeOptions = new ChromeOptions();
-            chromeOptions.AddArgument(SCREEN_RESOLUTION);
-            chromeOptions.AddArgument(INCOGNITO);
-            return new ChromeDriver(chromeOptions);
-        }
-
-        private static EdgeDriver GetEdgeDriverWithOptions()
-        {
-            EdgeOptions edgeOptions = new EdgeOptions();
-            edgeOptions.AddArgument(SCREEN_RESOLUTION);
-            edgeOptions.AddArgument(INCOGNITO);
-            return new EdgeDriver(edgeOptions);
-        }
-
-        private static FirefoxDriver GetFirefoxDriverWithOptions()
-        {
-            FirefoxOptions firefoxOptions = new FirefoxOptions();
-            firefoxOptions.AddArgument(SCREEN_RESOLUTION);
-            firefoxOptions.AddArgument(INCOGNITO);
-            return new FirefoxDriver(firefoxOptions);
-        }
+        private static IWebDriver InitializeWebDriver() => new DriverFactory().GetDriver();
 
         public void QuitDriver()
         {
